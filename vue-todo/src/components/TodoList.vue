40c2882e7bc741beb398641c877c1cdc0d425787
Jo-HyeRin <sayhyelin12@naver.com>
2023-02-15 16:21:25 +0900

    TodoList 로컬스토리지 데이터 가져와서 리스트 보여주기

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,12 +1,31 @@
 <template>
   <div>
-    list
+    <ul>
+        <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+            {{ todoItem }}
+        </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-
+    data: function() {
+        return {
+            todoItems: []
+        }
+    },
+    created: function() {
+        // 로컬스토리지 값 가져우기
+        if(localStorage.length > 0) {
+            for(var i = 0; i < localStorage.length ; i++) {
+                if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+                    // 로컬스토리지 값 확인하기 : console.log(localStorage.key(i));
+                    this.todoItems.push(localStorage.key(i));                
+                }
+            }
+        }
+    }
 }
 </script>
 
